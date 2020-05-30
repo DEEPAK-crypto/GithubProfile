6fb785dd0a73ab6d200d7a7673b1437694188291
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-30 22:28:00 +0530

    added api

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ app.set("view engine", 'ejs');
 app.use(express.static("public"));
 
 app.get('/', function(req, res) {
-    res.send('App Created');
+    const url = {
+        url: 'https://api.github.com/users/DEEPAK-crypto/repos',
+        headers: {
+            'User-Agent': 'DEEPAK-crypto'
+        }
+    }
+    request(url, function(error, request, body) {
+        var data = JSON.parse(body);
+
+        res.render('index', { data: data });
+
+    });
 });
 
 
