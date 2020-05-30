b92cafdcea360f57575666598bcfefaecf7b7949
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-30 23:36:47 +0530

    user info

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ request = require('request');
 app.set("view engine", 'ejs');
 app.use(express.static("public"));
 
+var data3;
+
+var url2 = {
+    url: 'https://api.github.com/users/DEEPAK-crypto/repos',
+    headers: {
+        'User-Agent': 'DEEPAK-crypto'
+    }
+}
+request(url2, function(error, request, body) {
+    data3 = JSON.parse(body);
+
+});
+
 app.get('/', function(req, res) {
     const url = {
-        url: 'https://api.github.com/users/DEEPAK-crypto/repos',
+        url: 'https://api.github.com/users/DEEPAK-crypto',
         headers: {
             'User-Agent': 'DEEPAK-crypto'
         }
@@ -15,7 +28,7 @@ app.get('/', function(req, res) {
     request(url, function(error, request, body) {
         var data = JSON.parse(body);
 
-        res.render('index', { data: data });
+        res.render('index', { user: data });
 
     });
 });
