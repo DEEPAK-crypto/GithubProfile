95028ddc87eb86f7abb331fa20eefda1aeef901c
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-06-01 15:14:35 +0530

    added search page

--- app.js
+++ app.js
@@ -4,22 +4,26 @@ axios = require("axios");
 app.set("view engine", 'ejs');
 app.use(express.static("public"));
 
-app.get('/user/:id', function(req, res) {
+app.get('/', function(req, res) {
+    res.render('index');
+})
 
+app.get('/user', function(req, res) {
+    var name = req.query.search;
     var user = axios({
         method: 'get',
-        url: 'https://api.github.com/users/' + req.params.id,
+        url: 'https://api.github.com/users/' + name,
         headers: {
-            'User-Agent': req.params.id
+            'User-Agent': name
         }
 
     });
 
     var repo = axios({
         method: 'get',
-        url: 'https://api.github.com/users/' + req.params.id + '/repos',
+        url: 'https://api.github.com/users/' + name + '/repos',
         headers: {
-            'User-Agent': req.params.id
+            'User-Agent': name
         }
 
     });
@@ -27,7 +31,7 @@ app.get('/user/:id', function(req, res) {
     axios.all([user, repo]).then(function(response) {
         var userData = response[0].data;
         var repoData = response[1].data;
-        res.render('index', { user: userData, repo: repoData });
+        res.render('result', { user: userData, repo: repoData });
 
     });
 });
