6a0f8a7f3ff77997da597bc6f06c99d44a0347e3
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-06-01 16:24:20 +0530

    added error handling

--- app.js
+++ app.js
@@ -29,10 +29,17 @@ app.get('/user', function(req, res) {
     });
 
     axios.all([user, repo]).then(function(response) {
-        var userData = response[0].data;
-        var repoData = response[1].data;
-        res.render('result', { user: userData, repo: repoData });
+        console.log(response[0].status)
+        if (response[0].status == 200 && response[1].status == 200) {
+            var userData = response[0].data;
+            var repoData = response[1].data;
+            res.render('result', { user: userData, repo: repoData });
+        } else
+            res.render('err');
 
+    }).catch(function(error) {
+        if (error)
+            res.render('err');
     });
 });
 
